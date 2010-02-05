b32cee92fe5aeb7abda9da6f7b3f1d491e28be86
Moxley Stratton <moxley@moxleydata.com>
2010-02-05 13:37:38 -0800

    Add installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+home=home
+
+cd
+ln -s $home/.ctags .ctags
+ln -s $home/.emacs .emacs
+ln -s $home/.emacs.d .emacs.d
+ln -s $home/.vim .vim
+test -f .vimrc && mv .vimrc .vimrc-bak
+ln -s $home/.vimrc .vimrc
+ln -s $home/bin bin
+
