31c78fdd73573e5871d71498562e0e77484dfd1c
Moxley Stratton <moxley.stratton@gmail.com>
2019-05-18 10:01:56 -0700

    Delete ctags and zsh

--- install.sh
+++ install.sh
@@ -3,12 +3,10 @@
 home=home
 
 cd
-ln -s $home/.ctags .ctags
 ln -s $home/.emacs .emacs
 ln -s $home/.emacs.d .emacs.d
 ln -s $home/.vim .vim
 test -f .vimrc && mv .vimrc .vimrc-bak
 ln -s $home/.vimrc .vimrc
-ln -s $home/.zshrc .zshrc
 ln -s $home/bin bin
 
