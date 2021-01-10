4deda281481c58ef235a7213467b6d32eb10f1fd
Moxley Stratton <moxley.stratton@gmail.com>
2021-01-10 15:10:09 -0800

    Adjust install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,8 @@
 home=home
 
 cd
-ln -s $home/.emacs .emacs
-ln -s $home/.emacs.d .emacs.d
-ln -s $home/.vim .vim
-test -f .vimrc && mv .vimrc .vimrc-bak
-ln -s $home/.vimrc .vimrc
 ln -s $home/bin bin
-
-echo "source \$HOME/home/.prompt" >> .bash_profile
-echo >> .bash_profile
-cat $home/aliases >> $HOME/.bash_profile
+ln -S $home/.zshrc ~/.zshrc
 
 git config --global user.name "Moxley Stratton"
 git config --global user.email "moxley.stratton@gmail.com"
